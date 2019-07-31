aa0f6bafc7e4aff3049241d12a585dfb542a7dc8
Patrick Harrison <jahman@bellatlantic.net>
2019-07-31 14:50:21 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,7 +23,7 @@ def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
-                  "oil paintings" => 3
+                  "oil paintings" => 3,
                  }
 shipping_manifest = 3
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
